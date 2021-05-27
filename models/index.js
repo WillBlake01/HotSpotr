e6469d193118640339daf5808bb04146f41f361d
William Blake <gwilliamblake@gmail.com>
2021-05-27 15:57:18 -0500

    Refactor passport user authentication

--- models/index.js
+++ models/index.js
@@ -7,12 +7,14 @@ var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/../config/config.js')[env];
 var db = {};
 
+let sequelize;
+
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else if (process.env.DATABASE_PRODUCTION) {
   sequelize = new Sequelize(process.env.DATABASE_PRODUCTION);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
